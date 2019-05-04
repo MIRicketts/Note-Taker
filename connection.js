971719d53c201629a7cdb68db83ef51265704b32
Marsden Ricketts <marsdenrickett@gmail.com>
2019-05-04 18:05:05 -0400

    syntax error with mysql

---
+++ connection.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+
+let connection;
+
+// Sets up db to connect locally or on JAWSDB if deployed
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "82ithemar",
+    database: "noteTaker_db"
+  });
+}
+
+// Turns BOOLEAN 0s and 1s returned from the db into true and false
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; 
+  }
+  return next();
+};
+
+// Export the connection so it's available in other parts of the app
+module.exports = connection;
--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const PORT = process.env.PORT || 3000;
 
 // set middleware to handle incoming POST data
 // for incoming json data
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 // set up routes
-app.get("/index", function(req, res){
+app.get("/index", function (req, res) {
   res.sendFile(path.join)(__dirname, "./index.html")
 });
 
-app.get("/notes", function(req, res){
+app.get("/notes", function (req, res) {
   // send notes.html when user hits "/notes"
   res.sendFile(path.join(__dirname, "./notes.html"))
 });
@@ -24,16 +26,44 @@ app.get("/notes", function(req, res){
 // API routes
 
 // GET info from table
-app.get("/api/notes", function (req,res){
+app.get("/api/notes", function (req, res) {
   // query(ask) database for table data
-  connection.query("SELECT * FROM notes", function (err, notesData){
-    if (err){
+  connection.query("SELECT * FROM notes", function (err, notesData) {
+    if (err) {
       // 500 error for database issue. based on the query from the database
       return res.status(500).json(err);
     }
+    // if not error, send back array of data
+    res.json(notesData);
+  });
+});
 
-  })
-})
+// POST route that takes in req.body and creates a new message
+app.post("/api/notes", function (req, res) {
+  // retrieve from notes and post message 
+  connection.query("SELECT COUNT(*)", function (err, idData) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+
+
+    // insert into new table using req.body 
+    connection.query("INSERT INTO notes SET ?", req.body, function (err, insertInfo) {
+      if (err) {
+        return res.status(400).json(err);
+      }
+      // if info inserted correctly
+      res.json({
+        status: "All Good"
+      })
+    });
+  });
+});
+
+app.get("*", function(req, res){
+  res.send("<h1> 404 Error!<h1>");
+});
 
 // turn on server
 // Always last in the app
